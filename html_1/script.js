2a775cf6648dfb258f1561974c36a87917fb1096
FredyC1 <alexcabreraap@icloud.com>
2024-07-31 14:10:24 -0400

    Worked on the flash card generator and did some javascript

--- html_1/script.js
+++ html_1/script.js
@@ -4,3 +4,11 @@ document.getElementById("gen-BTN").onclick = function() {
     username = document.getElementById("myText").ariaValueMax;
     console.log(username)
 }
+
+document.getElementById('Add-card-BTN').addEventListener('click', function() {
+    var originalDiv = document.querySelector('duplicate-div');
+
+    var clonedDiv = originalDiv.cloneNode(true); 
+
+    document.getElementById('flash-card-gen').appendChild(clonedDiv);
+});
