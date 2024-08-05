287254f4a1f17abd11779d1a16693351ac537ee5
FredyC1 <alexcabreraap@icloud.com>
2024-08-05 14:11:38 -0400

    I did javascript

--- html_1/script.js
+++ html_1/script.js
@@ -1,4 +1,4 @@
-let username;
+ /*let username;
 
 document.getElementById("gen-BTN").onclick = function() {
     username = document.getElementById("myText").ariaValueMax;
@@ -12,3 +12,52 @@ document.getElementById('Add-card-BTN').addEventListener('click', function() {
 
     document.getElementById('flash-card-gen').appendChild(clonedDiv);
 });
+*/
+// Animation
+var x = document.getElementById("login");
+var y = document.getElementById("signup");
+var z = document.getElementById("btn");
+var a = document.getElementById("signup-title")
+var b = document.getElementById("login-title")
+const changeColor = document.getElementById("signup-text-change")
+const changeColor2 = document.getElementById("login-text-change")
+function signup() {
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+    a.style.left = "0px";
+    b.style.left = "-400px";
+    a.style.visibility = "visible";
+    changeColor2.style.color = "#3499D1";
+    changeColor.style.color = "white";
+}
+
+function login() {
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0px";
+    a.style.left = "300px";
+    b.style.left = "0";
+    changeColor2.style.color = "white";
+    changeColor.style.color = "#3499D1";
+}
+
+//Pop Up
+
+function popUp() {
+    /*var blur = document.getElementById('blur')
+    blur.classList.popUp('active')*/
+
+    var popup = document.getElementById("popup");
+    popup.classList.toggle("active");
+};
+
+
+function addCard() {
+    var orignal = document.getElementById("duplicate-div");
+    var clone = orignal.cloneNode(true);
+
+    document.getElementById("flash-card-gen-whole").appendChild(clone);
+
+
+};
