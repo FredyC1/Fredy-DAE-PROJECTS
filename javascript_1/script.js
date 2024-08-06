22904722d27875585dbd4207a73cf33d5fe35859
FredyC1 <alexcabreraap@icloud.com>
2024-08-06 14:16:21 -0400

    added javascript

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -14,30 +14,30 @@ document.getElementById('Add-card-BTN').addEventListener('click', function() {
 });
 */
 // Animation
-var x = document.getElementById("login");
-var y = document.getElementById("signup");
-var z = document.getElementById("btn");
-var a = document.getElementById("signup-title")
-var b = document.getElementById("login-title")
-const changeColor = document.getElementById("signup-text-change")
-const changeColor2 = document.getElementById("login-text-change")
+var loginBTN = document.getElementById("login");
+var signupBTN = document.getElementById("signup");
+var BTN = document.getElementById("btn");
+var signupTitle = document.getElementById("signup-title");
+var loginTitle = document.getElementById("login-title");
+const changeColor = document.getElementById("signup-text-change");
+const changeColor2 = document.getElementById("login-text-change");
 function signup() {
-    x.style.left = "-400px";
-    y.style.left = "50px";
-    z.style.left = "110px";
-    a.style.left = "0px";
-    b.style.left = "-400px";
-    a.style.visibility = "visible";
+    loginBTN.style.left = "-400px";
+    signupBTN.style.left = "50px";
+    BTN.style.left = "110px";
+    signupTitle.style.left = "0px";
+    loginTitle.style.left = "-400px";
+    signupTitle.style.visibility = "visible";
     changeColor2.style.color = "#3499D1";
     changeColor.style.color = "white";
 }
 
 function login() {
-    x.style.left = "50px";
-    y.style.left = "450px";
-    z.style.left = "0px";
-    a.style.left = "300px";
-    b.style.left = "0";
+    loginBTN.style.left = "50px";
+    signupBTN.style.left = "450px";
+    BTN.style.left = "0px";
+    signupTitle.style.left = "300px";
+    loginTitle.style.left = "0";
     changeColor2.style.color = "white";
     changeColor.style.color = "#3499D1";
 }
@@ -53,11 +53,40 @@ function popUp() {
 };
 
 
+// duplicate
+let currentNumber = 5; 
+
 function addCard() {
-    var orignal = document.getElementById("duplicate-div");
-    var clone = orignal.cloneNode(true);
+    var original = document.getElementById("duplicate-div");
+    var clone = original.cloneNode(true);
+
+    currentNumber++;
+
+    const newNumberDiv = clone.querySelector("#number-change");
+    newNumberDiv.textContent = currentNumber;
+    newNumberDiv.id = "";
 
     document.getElementById("flash-card-gen-whole").appendChild(clone);
+} 
 
+// Title's change
 
+let title = ''
+
+/* function changeTitle() {
+    const titleInput = document.getElementById('input-text').value;
+    const pageTitle = document.getElementById('flash-card-title');
+    pageTitle.textContent = titleInput;
+} */
+
+function changeTitle() {
+    const titleInput = document.getElementById('title-input').value;
+    localStorage.setItem('flash-card-title', titleInput);
+}
+
+window.onload = function() {
+    const savedTitle = localStorage.getItem('flash-card-title');
+    if (savedTitle) {
+        document.getElementById('flash-card-title').textContent = savedTitle;
+    }
 };
